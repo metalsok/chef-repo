25980966fcecf3fcf1e7b8e4df8903d9da5e88f1
Sokratis <sokratis.metallidis@bytesource.net>
2014-03-18 11:01:52 +0100

    mysql database

--- cookbooks/phpapp/recipes/default.rb
+++ cookbooks/phpapp/recipes/default.rb
@@ -17,3 +17,9 @@ include_recipe "apache2::mod_php5"
 apache_site "default" do 
 enable true
 end
+
+
+mysql_database node['phpapp']['database'] do 
+connection ({:host=>'localhost',:username=>'root',:password=>node['mysql']['server_root_password']})
+action:create
+end
